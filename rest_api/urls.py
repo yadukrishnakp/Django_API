f2cb6dc068298dfbc3bb19c26c5ee9b429f13584
yadukrishnakp <yadukrishnakp007@gmail.com>
2022-04-22 15:24:58 +0530

    Two API's are created one for show all employee details and another for show details of a specific employee

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('', views.Show_Employee, name="show-employees")
-    path('create_employee/', views.Create_Employee, name="create-employee")
+    path('create_employee/', views.Create_Employee, name="create-employee"),
+    path('show_all_employees/', views.Show_all_Employees, name="show-all-employee"),
+    path('show_employee/<str:pk>', views.Show_Employee, name="create-employee"),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -14,3 +14,19 @@ def Create_Employee(request):
     if serializer.is_valid():
         serializer.save()
     return Response("Employee added successfully")
+
+
+# show all employees details
+@api_view(['GET'])
+def Show_all_Employees(request):
+    employees = Employee.objects.all()
+    serializer = Employee_Serializer(employees, many=True)
+    return Response(serializer.data)
+
+
+# show one employee details
+@api_view(['GET'])
+def Show_Employee(request, pk):
+    employees = Employee.objects.get(id=pk)
+    serializer = Employee_Serializer(employees, many=False)
+    return Response(serializer.data)
